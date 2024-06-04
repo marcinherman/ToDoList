1d30fcc6c43adacb74c2659c2e6a096078eaa00a
herman <herman@primesoft238.local>
2024-06-04 20:49:54 +0200

    cleaning

--- to_todo_list/lib/main.dart
+++ to_todo_list/lib/main.dart
@@ -9,11 +9,11 @@ void main() {
   
 }
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key }) :super(key: key);
+  const MyApp({super.key }) ;
 
 @override
   Widget build(BuildContext context) {
-    return MaterialApp( 
+    return const MaterialApp( 
       debugShowCheckedModeBanner: false,
     title: 'ToDo App',
     home: Home());
