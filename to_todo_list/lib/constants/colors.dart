475948061bf71079341c776622c171b718605dc3
herman <herman@primesoft238.local>
2024-06-06 22:21:27 +0200

    add searchbar

--- to_todo_list/lib/constants/colors.dart
+++ to_todo_list/lib/constants/colors.dart
@@ -3,5 +3,5 @@ import "package:flutter/material.dart";
 const Color tdRed = Color(0xffFF0000);
 const Color tdBlue = Color(0xff0000FF);
 const Color tdBlack = Color(0xff000000);
-const Color tdwGrey = Color(0xff717171);
-const Color tdbColor = Color(0xFFEEEFF5);
+const Color tdGrey = Color(0xff717171);
+const Color tdBGColor = Color(0xFFEEEFF5);
--- to_todo_list/lib/screens/home.dart
+++ to_todo_list/lib/screens/home.dart
@@ -2,23 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:to_todo_list/constants/colors.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(children:[
-        Icon(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            searchBox(),
+            ListView(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: const TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(
+              Icons.search,
+              color: tdBlack,
+              size: 20,
+            ),
+            prefixIconConstraints: BoxConstraints(
+              maxHeight: 20,
+              maxWidth: 25,
+            ),
+            border: InputBorder.none,
+            hintText: "Search",
+            hintStyle: TextStyle(color: tdGrey)),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        const Icon(
           Icons.menu,
           color: tdBlack,
-          size:30,
-          )
-        ]),
-      ),
-      body: Container(
-        child: Text('This is the home screen.'),
-      ),
+          size: 30,
+        ),
+        Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/lion2.jpeg'),
+            )),
+      ]),
     );
   }
 }
